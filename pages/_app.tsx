d77046569538b140eeb2214664f773889ec00948
raizo <35413558+iraizo@users.noreply.github.com>
2020-06-12 23:54:04 +0200

    removed fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,6 @@ export default class MyApp extends App {
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                     <title>switchbla.de</title>
-                    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"></link>
                 </Head>
                 <Component {...pageProps} />
             </>
