9eefd70978636abe2df7c2031f0d7a6360233f3f
iraizo <groebner.loron@gmail.com>
2020-08-23 05:25:18 +0200

    :sparkle: fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
       require("tailwindcss"),
       require("autoprefixer"),
-        require('@fullhuman/postcss-purgecss')({
+        purgecss({
             content: ["./pages/**/*.tsx"],
             defaultExtractor: content =>  content.match(/[A-Za-z0-9-_:/]+/g) || []
         }),
