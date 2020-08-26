a3c22c2320c273626f787f44d4c2906bb94427b6
iraizo <groebner.loron@gmail.com>
2020-08-26 14:43:09 +0200

    -

---
+++ nextjs.config.js
@@ -0,0 +1,9 @@
+const withCss = require('@zeit/next-css')
+const withPurgeCss = require('next-purgecss')
+
+
+module.exports = withCss(withPurgeCss({
+    purgeCssPaths: [
+        'pages/**/*'
+    ]
+}))
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
     plugins: [
-      require("tailwindcss"),
-      require("autoprefixer"),
-        purgecss({
-            content: ["./pages/**/*.tsx"],
-            defaultExtractor: content =>  content.match(/[A-Za-z0-9-_:/]+/g) || []
-        }),
-    ],
+        "tailwindcss", "autoprefixer"
+    ]
 }
